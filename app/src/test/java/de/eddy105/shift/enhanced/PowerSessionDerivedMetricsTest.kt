19063aca172e0f2e3c7e8dfdb3ec049f19850275
Edward Tillmann <eddy458@gmx.de>
2026-08-26 14:39:20 +0200

    test: fix nullable capacity delta assertion

--- app/src/test/java/de/eddy105/shift/enhanced/PowerSessionDerivedMetricsTest.kt
+++ app/src/test/java/de/eddy105/shift/enhanced/PowerSessionDerivedMetricsTest.kt
@@ -17,7 +17,7 @@ class PowerSessionDerivedMetricsTest {
         assertEquals(3, metrics.sampleCount)
         assertEquals(-900, metrics.minimumCurrentMilliamps)
         assertEquals(-400, metrics.maximumCurrentMilliamps)
-        assertEquals(-2, metrics.capacityDeltaPercent?.toInt())
+        assertEquals(-2, metrics.capacityDeltaPercent ?: error("capacity delta should be available"))
         assertEquals(31.0, metrics.averageTemperatureCelsius, 0.001)
     }
 
